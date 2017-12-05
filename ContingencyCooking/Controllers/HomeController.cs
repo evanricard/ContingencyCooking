5dca7d6306e348e70c3341b1496498782fc08400
Evan Ricard <evanricard11@gmail.com>
2017-12-05 13:26:06 -0500

    View edits for landing page/edit

--- ContingencyCooking/Controllers/HomeController.cs
+++ ContingencyCooking/Controllers/HomeController.cs
@@ -21,15 +21,12 @@ namespace ContingencyCooking.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-            string apikey = ConfigurationManager.AppSettings["BigOven"];
+            //string apikey = ConfigurationManager.AppSettings["BigOven"];
             return View();
         }
         public ActionResult Search()
